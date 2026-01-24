64676c594b82e5e5e1858f9866ee9577487989e2
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-01-24 19:11:13 +0530

    Update email and password handling in Non Shall Pass script

--- ruby-course/non_shall_pass.rb
+++ ruby-course/non_shall_pass.rb
@@ -1,2 +1,28 @@
-EMAIL="chamikau@example.com"
-USERNAME="chamika-u"
+# logging details
+EMAIL="pakaya@example.com"
+PASSWORD="Pakaya@123"
+
+# welcome messages and login instructions
+puts "Welcome to Non Shall Pass - A Password Manager"
+puts "Please, sign in to continue"
+print "Enter Emial: "
+
+# get input as user_email for EMAIL
+user_email=gets.chomp
+
+# check whether user input is corect or wrong
+if user_email==EMAIL
+  print "Enter Password: "
+  user_password=gets.chomp
+else
+  puts "Invaild Email"
+  exit
+end
+
+# if password is right then these messages will display 
+unless user_password != PASSWORD
+  puts "Hello, #{user_email}."
+  puts "What whould you like ro do?"
+  puts "1. Add service credentials"
+  puts "2. Retrieve and existing service credentials"
+end
